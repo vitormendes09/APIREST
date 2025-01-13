bb9f4c800352b840463fadb31652575d726742c7
Vitor <mendes.v@gsuite.iff.edu.br>
2025-01-13 14:46:36 -0300

    Adicionando Rotas

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
-    @Get()
+    
+    @Get()  
     findAll() {
         return 'This action returns all recados';
     }
@@ -12,4 +13,26 @@ export class RecadosController {
         console.log(id);
         return `This action returns a #${id} recado`;
     }
+
+    @Post()
+    create(@Body() createRecadoDto: any) {
+        return createRecadoDto;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateRecadoDto: any) {
+        return {
+            id,
+            ...updateRecadoDto, 
+        };
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: String){
+        return 'This action removes a recado';
+    }
+
+    
 }
+
+
