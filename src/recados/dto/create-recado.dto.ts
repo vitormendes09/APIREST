ffc56f213e31331e03bd5e9fa14aec53a62677d1
Vitor <mendes.v@gsuite.iff.edu.br>
2025-01-15 23:29:38 -0300

    Validação

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,19 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 export class CreateRecadoDto{
+  
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  @MaxLength(100)
  readonly texto: string;
+ @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(100)
  readonly de: string;
+ @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(100)
  readonly para: string
 }
